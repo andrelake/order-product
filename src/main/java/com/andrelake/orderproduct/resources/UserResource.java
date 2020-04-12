ab75e5aa921275fcb59b806076e3c2eb2a7c12e7
andrelake <andre.lake@hotmail.com>
2020-04-12 15:49:26 -0300

    User delete

--- src/main/java/com/andrelake/orderproduct/resources/UserResource.java
+++ src/main/java/com/andrelake/orderproduct/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -43,5 +44,12 @@ public class UserResource {
 		
 		user = service.insert(user);
 		return ResponseEntity.status(HttpStatus.CREATED).body(user);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
