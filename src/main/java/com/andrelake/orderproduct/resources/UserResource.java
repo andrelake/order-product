db1ff06133193a152b2f63e08cf998b8f63dccb0
andrelake <andre.lake@hotmail.com>
2020-04-12 15:42:41 -0300

    User insert

--- src/main/java/com/andrelake/orderproduct/resources/UserResource.java
+++ src/main/java/com/andrelake/orderproduct/resources/UserResource.java
@@ -3,9 +3,12 @@ package com.andrelake.orderproduct.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,5 +36,12 @@ public class UserResource {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 		
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User user) {
+		
+		user = service.insert(user);
+		return ResponseEntity.status(HttpStatus.CREATED).body(user);
 	}
 }
